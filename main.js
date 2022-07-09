48ca17c81acb8a68616aa2f461617837b115816a
aamnasuhail <108998979+aamnasuhail@users.noreply.github.com>
2022-07-10 01:01:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var inputValue = document.getElementById("inputValue");
+let btn = document.getElementById("btn");
+let nameCity = document.getElementById("nameCity");
+let weather = document.getElementById("weather");
+let desc = document.getElementById("desc");
+let temp = document.getElementById("temp");
+
+btn.addEventListener("click", function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      inputValue.value +
+      "&appid=ff092519bc84a7badf9349529e5ce1d5"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // let city = data["name"];
+      // let weather = data["weather"]["main"];
+      // let humidity = data["main"]["humidity"];
+      // let tempr = data["main"]["temp"];
+      var tempC = data.main.temp - 273.15;
+      tempC = tempC.toFixed(2);
+
+      nameCity.innerHTML = `City - ${data.name}`;
+      weather.innerHTML = `Weather - ${data.weather[0].description} `;
+      desc.innerHTML = `Humidity - ${data.main.humidity}`;
+      temp.innerHTML = `Temperature - ${tempC}ºC`;
+    })
+    .catch((err) => {
+      console.log("bro you are entered wrong city");
+    });
+});
+
+// "https://api.openweathermap.org/data/2.5/weather?q=" +
+//       inputValue.value +
+//       "&appid=ff092519bc84a7badf9349529e5ce1d5"
